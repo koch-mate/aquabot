1df391ff5f1062a5c1dfde1230aa52e1264f6559
koch-mate <koch.mate@icloud.com>
2025-10-05 16:09:26 +0200

    skeleton

---
+++ main.py
@@ -0,0 +1,45 @@
+import RPi.GPIO as GPIO
+import time
+
+from conf import pins
+from lib import common
+
+from transitions import Machine
+
+# setup GPIO
+#
+
+GPIO.setmode(GPIO.BCM)
+
+for s in pins.SWITCHES:
+    GPIO.setup(s, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+for l in pins.LEDS:
+    GPIO.setup(l, GPIO.OUT)
+    GPIO.output(l, GPIO.LOW)
+
+
+while True:
+    time.sleep(0.02)
+    GPIO.output(pins.LEF, GPIO.input(pins.SWB))
+    GPIO.output(pins.LER, GPIO.input(pins.SWTCCW))
+
+
+class Robot:
+    states = ["off", "boot", "calibrate", "idle", "move"]
+
+    def __init__(self, name):
+        self.name = name
+        self.machine = Machine(model=self, states=Robot.states, initial="off")
+
+        self.machine.add_transition("start", "off", "boot")
+        self.machine.add_transition("bootReady", "boot", "calibrate")
+        self.machine.add_transition("calibrateOk", "calibrate", "idle")
+        self.machine.add_transition("calibrateOk", "calibrate", "off")
+        self.machine.add_transition("shutdown", "*", "off")
+
+    def run(self):
+        pass
+
+
+robot = Robot("aquabot")
