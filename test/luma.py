4b99b604e9723eed4dee6eca1bd81f9890a4056f
koch-mate <koch.mate@icloud.com>
2025-10-05 17:35:08 +0200

    Font test

--- test/luma.py
+++ test/luma.py
@@ -3,11 +3,14 @@ from luma.core.render import canvas
 from luma.oled.device import ssd1306, ssd1325, ssd1331, sh1106
 from time import sleep
 
+from PIL import Image, ImageDraw, ImageFont
+
 serial = i2c(port=1, address=0x3C)
 device = ssd1306(serial, height=32, rotate=2)
 
 # Box and text rendered in portrait mode
 with canvas(device) as draw:
-    draw.rectangle(device.bounding_box, outline="white", fill="black")
-    draw.text((10, 40), "Hello World", fill="white")
+    # draw.rectangle(device.bounding_box, outline="white", fill="black")
+    font = ImageFont.truetype("tiny.ttf", 6)
+    draw.text((2, 2), "Hello World", fill="white", font=font)
 sleep(10)
